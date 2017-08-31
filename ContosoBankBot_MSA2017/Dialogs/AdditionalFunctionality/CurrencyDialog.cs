4cc059b7aa8bcd630a4df4359d60f7c124cbcad9
Aristarkh Tikhonov <Aristarkh4@gmail.com>
2017-08-31 17:38:11 +1200

    Construct a currency exchange dialog. Call a fixer.io API to get exchange rate.

--- ContosoBankBot_MSA2017/Dialogs/AdditionalFunctionality/CurrencyDialog.cs
+++ ContosoBankBot_MSA2017/Dialogs/AdditionalFunctionality/CurrencyDialog.cs
@@ -1,30 +1,101 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.Bot.Builder.Dialogs;
+﻿using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ContosoBankBot_MSA2017.Dialogs.AdditionalFunctionality
 {
     [Serializable]
     public class CurrencyDialog : IDialog<object>
     {
-        public Task StartAsync(IDialogContext context)
-        {
-            context.Wait(MessageReceivedAsync);
+        private string baseCurrency = "EUR";
+        private string goalCurrency = "EUR";
 
-            return Task.CompletedTask;
+        public async Task StartAsync(IDialogContext context)
+        {
+            await context.PostAsync("What currency rate do you want to know? (USD, GBP, etc.)");
+            context.Wait(MessageReceivedAsync);
         }
 
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
         {
             var activity = await result as Activity;
+            goalCurrency = activity.Text;
 
-            // calculate something for us to return
-            int length = (activity.Text ?? string.Empty).Length;
+            await AfterGoalCurrencyReceivedAsync(context);
+        }
 
-            // return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+        private async Task AfterGoalCurrencyReceivedAsync(IDialogContext context)
+        {
+            try
+            {
+                PromptDialog.Confirm(
+                    context,
+                    AfterAcceptingChangeOfBaseCurrencyAsync,
+                    "Current base currency is " + baseCurrency + ". Do you want to change base currency ?");
+            }
+            catch (TooManyAttemptsException e)
+            {
+                await context.PostAsync("You attempted too many times, please enter the goal currency again.");
+                context.Wait(MessageReceivedAsync);
+            }
+        }
 
+        private async Task AfterAcceptingChangeOfBaseCurrencyAsync(IDialogContext context, IAwaitable<bool> result)
+        {
+            var isBaseCurrencyChanging = await result;
+            if (isBaseCurrencyChanging)
+            {
+                try
+                {
+                    PromptDialog.Text(
+                        context,
+                        AfterReceivingNewBaseCurrencyAsync,
+                        "Enter the new base currency.");
+                }
+                catch (TooManyAttemptsException e)
+                {
+                    await context.PostAsync("You attempted too many times, please enter the goal currency again.");
+                    context.Wait(MessageReceivedAsync);
+                }
+            }
+            else
+            {
+                await GetGoalCurrencyRateAsync(context);
+            }
+        }
+
+        private async Task AfterReceivingNewBaseCurrencyAsync(IDialogContext context, IAwaitable<string> result)
+        {
+            baseCurrency = await result;
+            await GetGoalCurrencyRateAsync(context);
+        }
+
+        private async Task GetGoalCurrencyRateAsync(IDialogContext context)
+        {
+            await context.PostAsync("Got it. Give me a second to get the latest exchange rate.");
+
+            string url = "http://api.fixer.io/latest?base=" + baseCurrency + "&symbols=" + goalCurrency;
+            /*try
+            {*/
+
+            string responseString = null;
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                responseString = await response.Content.ReadAsStringAsync();
+            }
+
+            /* }
+             catch { }*/
+
+            await context.PostAsync("What currency rate do you want to know? (USD, GBP, etc.)");
             context.Wait(MessageReceivedAsync);
         }
     }
