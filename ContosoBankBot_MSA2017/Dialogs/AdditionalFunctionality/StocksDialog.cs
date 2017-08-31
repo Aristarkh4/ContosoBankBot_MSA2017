d29660d834f398ec985c21ddf29bfcf937735156
Aristarkh Tikhonov <Aristarkh4@gmail.com>
2017-08-31 22:43:21 +1200

    Add unaviability messages.

--- ContosoBankBot_MSA2017/Dialogs/AdditionalFunctionality/StocksDialog.cs
+++ ContosoBankBot_MSA2017/Dialogs/AdditionalFunctionality/StocksDialog.cs
@@ -8,24 +8,15 @@ namespace ContosoBankBot_MSA2017.Dialogs.AdditionalFunctionality
     [Serializable]
     public class StocksDialog : IDialog<object>
     {
-        public Task StartAsync(IDialogContext context)
+        public async Task StartAsync(IDialogContext context)
         {
-            context.Wait(MessageReceivedAsync);
-
-            return Task.CompletedTask;
+            await context.PostAsync("Sorry this functionality is not yet implemented.");
+            context.Done<object>(null);
         }
 
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
         {
-            var activity = await result as Activity;
-
-            // calculate something for us to return
-            int length = (activity.Text ?? string.Empty).Length;
-
-            // return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
-
-            context.Wait(MessageReceivedAsync);
+            
         }
     }
 }
